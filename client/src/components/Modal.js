841b895471940b403b62a82975372205c66cbbd2
anyajegorova <anna.jerm@outlook.com>
2023-03-06 21:52:57 +0200

    creating modal for editing and creating

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,68 @@
-function Modal() {
-    return (
-      <div >
-  
-      </div>
-    );
+import { useState } from 'react';
+
+function Modal({ mode, setShowModal, task }) {
+  const editMode = mode === 'edit' ? true : false;
+  const [data, setData] = useState({
+    user_email: editMode ? task.user_email : null,
+    title: editMode ? task.title : null,
+    progress: editMode ? task.progress : 50,
+    date: editMode ? '' : new Date()
+  })
+
+  const postData = () => {
+    try {
+      fetch()
+    } catch (err) {
+      err => console.err(err)
+    }
   }
-  
-  export default Modal;
-  
+
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData(data => ({
+      ...data,
+      [name]: [value]
+    }))
+  }
+  return (
+    <div className='overlay'>
+      <div className='modal'>
+        <div className='form-title-container'>
+          <h3>Let's {mode} your task</h3>
+          <button onClick={() => setShowModal(false)}>X</button>
+
+        </div>
+
+        <form>
+          <input
+            required
+            maxLength={30}
+            placeholder='Your task goes here'
+            name='title'
+            value={data.title}
+            onChange={handleChange}
+          />
+          <br />
+          <label for='range'>Drag to select your current progress</label>
+          <input
+            required
+            type='range'
+            id='range'
+            min='0'
+            max='100'
+            name='progress'
+            value={data.progress}
+            onChange={handleChange}
+          />
+          <input className={mode} type='submit' />
+
+        </form>
+
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
